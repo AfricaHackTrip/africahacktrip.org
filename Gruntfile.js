aa5bae94d7f0a2eb440343d38c9085265f8cadec
Sebastian Kippe <sebastian@kip.pe>
2013-08-04 00:05:31 +0200

    Fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -185,7 +185,7 @@ module.exports = function (grunt) {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+                        '<%= yeoman.dist %>/images/*.{png,jpg,jpeg,gif,webp}',
                         '<%= yeoman.dist %>/styles/fonts/*'
                     ]
                 }
@@ -267,7 +267,7 @@ module.exports = function (grunt) {
                     src: [
                         '*.{ico,txt}',
                         '.htaccess',
-                        'images/{,*/}*.{webp,gif}',
+                        'images/{,*/}*.{webp,gif,png,jpg,jpeg}',
                         'styles/fonts/*'
                     ]
                 }]
