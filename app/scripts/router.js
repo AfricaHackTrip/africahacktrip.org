abcc626ae42123fb0c61e70af9a251a53d781aef
Michael Bumann <michael@railslove.com>
2013-08-04 17:35:37 +0200

    initialize subpages

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,4 +3,8 @@ AHT.Router.map(function () {
   this.route('uganda',   { path: "/uganda" });
   this.route('rwanda',   { path: "/rwanda" });
   this.route('tanzania', { path: "/tanzania" });
+  this.route('contact', { path: "/contact" });
+  this.route('about-us', { path: "/about-us" });
+  this.route('supporters', { path: "/supporters" });
+  this.route('sponsoring', { path: "/sponsoring" });
 });
