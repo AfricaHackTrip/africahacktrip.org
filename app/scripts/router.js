518192b5453559f2f2576b16c0ab71d316776878
Sebastian Kippe <sebastian@kip.pe>
2013-08-05 16:16:36 +0200

    Improve layout for content pages

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -4,7 +4,6 @@ AHT.Router.map(function () {
   this.route('rwanda',   { path: "/rwanda" });
   this.route('tanzania', { path: "/tanzania" });
 
-  this.route('contact', { path: "/contact" });
   this.route('about-us', { path: "/about-us" });
   this.route('supporters', { path: "/supporters" });
   this.route('sponsoring', { path: "/sponsoring" });
