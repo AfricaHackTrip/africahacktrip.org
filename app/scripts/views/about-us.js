3c35655038106d83c6fe5a5651649f8854f9c656
Michael Bumann <michael@railslove.com>
2013-08-05 20:42:04 +0200

    copy the styles for the about page

--- app/scripts/views/about-us.js
+++ app/scripts/views/about-us.js
@@ -2,6 +2,14 @@ AHT.AboutUsView = Ember.View.extend({
 
     didInsertElement: function() {
       $('html, body').animate({scrollTop:0});
+      $("#The_team .toggle").on("click", function(e) {
+        console.log("ooo");
+        e.preventDefault();
+        $("#The_team article").removeClass("more");
+        console.log($(this));
+        $(this).closest("article").addClass("more");
+      });
+
     }
 
 });
