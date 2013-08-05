2adca4b6c7a58c50943197d12738444cc1c2b10e
Sebastian Kippe <sebastian@kip.pe>
2013-08-05 23:16:20 +0200

    Update build

--- dist/scripts/6b30c34f.main.js
+++ dist/scripts/6b30c34f.main.js
@@ -31,6 +31,15 @@ AHT.AboutUsView = Ember.View.extend({
 
     didInsertElement: function() {
       $('html, body').animate({scrollTop:0});
+      $("#The_team .toggle").on("click", function(e) {
+        e.preventDefault();
+        $("#The_team article").removeClass("more");
+        $(this).closest("article").addClass("more");
+        $('html, body').animate({
+          scrollTop: $(this).closest("article").offset().top - 50
+        }, 300);
+      });
+
     }
 
 });
