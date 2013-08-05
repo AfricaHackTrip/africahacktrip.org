3d010d288ef491c139faa9569aa42581708a7dae
Sebastian Kippe <sebastian@kip.pe>
2013-08-05 23:15:50 +0200

    Awesomify about page

--- app/scripts/views/about-us.js
+++ app/scripts/views/about-us.js
@@ -3,11 +3,12 @@ AHT.AboutUsView = Ember.View.extend({
     didInsertElement: function() {
       $('html, body').animate({scrollTop:0});
       $("#The_team .toggle").on("click", function(e) {
-        console.log("ooo");
         e.preventDefault();
         $("#The_team article").removeClass("more");
-        console.log($(this));
         $(this).closest("article").addClass("more");
+        $('html, body').animate({
+          scrollTop: $(this).closest("article").offset().top - 50
+        }, 300);
       });
 
     }
