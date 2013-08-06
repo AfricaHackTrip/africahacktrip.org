dd90100667250caf6b3b736866e498c7530f1f38
Michael Bumann <michael@railslove.com>
2013-08-07 00:13:38 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-grunt build && git add --all && git commit -m "Update build" && git subtree push --prefix dist 5apps master
+git pull origin master && grunt build && git add --all && git commit -m "Update build" && git subtree push --prefix dist 5apps master
