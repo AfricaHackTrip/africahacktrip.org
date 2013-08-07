716c7cba29cdd8da2016ca472c4d601395283a5d
Sebastian Kippe <sebastian@kip.pe>
2013-08-07 18:56:07 +0200

    Update build

--- dist/scripts/94316e52.main.js
+++ dist/scripts/94316e52.main.js
@@ -27,6 +27,27 @@ AHT.KenyaRoute = Ember.Route.extend({
 
 (function() {
 
+AHT.ApplicationController = Ember.Controller.extend({
+    currentPathChanged: function() {
+      var page;
+      Ember.run.next(function(){
+        if (!Ember.isNone(_paq)) {
+          page = window.location.hash.length > 0 ?
+                 window.location.hash.substring(1) :
+                 window.location.pathname;
+          page = page === '/' ? 'index' : page;
+
+          _paq.push(['trackPageView', page]);
+        }
+      });
+    }.observes('currentPath')
+});
+
+
+})();
+
+(function() {
+
 AHT.AboutUsView = Ember.View.extend({
 
     didInsertElement: function() {
