ca52ef8f361bb254bd55eb1b09afa6876012c4d3
Michael Bumann <michael@railslove.com>
2013-08-07 11:58:00 +0200

    push and pull all the time

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git pull origin master && grunt build && git add --all && git commit -m "Update build" && git subtree push --prefix dist 5apps master
+git pull origin master && git push origin master && grunt build && git add --all && git commit -m "Update build" && git subtree push --prefix dist 5apps master && git push origin master
